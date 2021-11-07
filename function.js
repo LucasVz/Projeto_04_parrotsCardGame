f8c0f0b5022d5b05795f2e5e8c680f72d61c3523
LucasCosta <lucastadeuvaz@gmail.com>
2021-11-07 15:34:36 -0300

    verificar pares incompleto

--- function.js
+++ function.js
@@ -1,4 +1,6 @@
   let quantidadeCartas;
+  let contador = 0;
+
   function qualQuantidadeCartas(){
   quantidadeCartas = parseInt(prompt("insira um numero de 4 a 14"));
   while (quantidadeCartas < 4 || quantidadeCartas > 14 || isNaN(quantidadeCartas) || quantidadeCartas % 2 !== 0){
@@ -12,9 +14,49 @@ function comparador() {
 
 function VirarCarta(carta){
   const viraVerso = carta.querySelector(".carta .verso-carta");
-  viraVerso.classList.toggle("ativar-verso");
+  viraVerso.classList.add("ativar-verso");
   const viraFrente = carta.querySelector(".carta .frente-carta");
-  viraFrente.classList.toggle("ativar-frente");
+  viraFrente.classList.add("ativar-frente");
+  contador ++;
+  console.log(contador);
+  
+  
+  let compararCarta1;
+  let compararCarta2;
+  let v;
+
+  if(contador === 1){
+    compararCarta1 = carta.querySelector(".frente-carta");
+    primeiroClick = compararCarta1.innerHTML;
+  }
+  if(contador === 2 ){
+    compararCarta2 = carta.querySelector(".frente-carta");
+    segundoClick = compararCarta2.innerHTML;
+    contador = 0;
+
+    if(primeiroClick === segundoClick){
+      viraVerso.classList.remove("ativar-verso");
+      viraFrente.classList.remove("ativar-frente");
+      viraVerso.classList.add(".achou-par-verso");
+      viraFrente.classList.add(".achou-par-frente");
+
+    }
+    else{
+      intervalo = setTimeout(virarCartasDiferentes, 2000);
+    }
+    console.log(v);
+  }
+  
+}
+
+function virarCartasDiferentes(){
+  const viraVerso = document.querySelectorAll(".carta .verso-carta");
+  const viraFrente = document.querySelectorAll(".carta .frente-carta");
+  for(let i = 0 ; i < quantidadeCartas; i++){
+    viraFrente[i].classList.remove("ativar-frente");
+    viraVerso[i].classList.remove("ativar-verso");
+    console.log(i);
+  }
 }
 
 function adicionarCartas(){
@@ -42,19 +84,21 @@ function adicionarCartas(){
 
   for(let i = 0; i < quantidadeCartas; i++){
   cartas.innerHTML += `
-  <div class="carta" onclick="VirarCarta(this)" data-identifier="carta">
+  <div class="carta" onclick="VirarCarta(this)" data-identifier="card">
     <div class="verso-carta" data-identifier="front-face">
       <img src="./imgs/front.png" alt="carta">
     </div>
 
     <div class="frente-carta" data-identifier="back-face">
-      <img src="./imgs/${copiaImagens[i]}.gif" alt="carta">
+      <img class="${copiaImagens[i]}" src="./imgs/${copiaImagens[i]}.gif" alt="carta">
     </div>
   </div> 
   `;
   }
 
 }
+
+
 qualQuantidadeCartas();
 adicionarCartas();
 imagens.sort(comparador);
