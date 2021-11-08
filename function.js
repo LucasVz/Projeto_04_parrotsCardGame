eaf00a29aa968b8e962642962229e2788fae6ffb
LucasCosta <lucastadeuvaz@gmail.com>
2021-11-08 15:13:51 -0300

    jogo completo

--- function.js
+++ function.js
@@ -1,61 +1,82 @@
   let quantidadeCartas;
   let contador = 0;
+  let compararCarta1;
+  let compararCarta2;
+  let compararCarta1Verso;
+  let compararCarta2Verso;
+  let contadorJogadas = 0;
+  let contarPares = 0;
+
+  function comparador() { 
+    return Math.random() - 0.5; 
+  }
 
   function qualQuantidadeCartas(){
-  quantidadeCartas = parseInt(prompt("insira um numero de 4 a 14"));
+  quantidadeCartas = parseInt(prompt("Insira um numero de 4 a 14 par"));
   while (quantidadeCartas < 4 || quantidadeCartas > 14 || isNaN(quantidadeCartas) || quantidadeCartas % 2 !== 0){
-  quantidadeCartas = parseInt(prompt("insira um numero de 4 a 14"));
+  quantidadeCartas = parseInt(prompt("Insira um numero de 4 a 14 par"));
   }
  }
-
-function comparador() { 
-	return Math.random() - 0.5; 
-}
 
 function VirarCarta(carta){
   const viraVerso = carta.querySelector(".carta .verso-carta");
   viraVerso.classList.add("ativar-verso");
   const viraFrente = carta.querySelector(".carta .frente-carta");
   viraFrente.classList.add("ativar-frente");
+
   contador ++;
-  console.log(contador);
-  
-  
-  let compararCarta1;
-  let compararCarta2;
-  let v;
+  contadorJogadas++;
 
   if(contador === 1){
     compararCarta1 = carta.querySelector(".frente-carta");
+    compararCarta1Verso = carta.querySelector(".verso-carta");
     primeiroClick = compararCarta1.innerHTML;
+    carta.classList.add("nao-clicavel-primeira-carta");
   }
-  if(contador === 2 ){
+
+  if(contador === 2){
     compararCarta2 = carta.querySelector(".frente-carta");
+    compararCarta2Verso = carta.querySelector(".verso-carta");
     segundoClick = compararCarta2.innerHTML;
-    contador = 0;
-
     if(primeiroClick === segundoClick){
-      viraVerso.classList.remove("ativar-verso");
-      viraFrente.classList.remove("ativar-frente");
-      viraVerso.classList.add(".achou-par-verso");
-      viraFrente.classList.add(".achou-par-frente");
+      carta.classList.add("nao-clicavel");
+      contarPares += 2;
+      console.log(contarPares);
+      setTimeout(ganharPartida, 200);
+    }
 
-    }
     else{
-      intervalo = setTimeout(virarCartasDiferentes, 2000);
+      adicionarNaoClicavel()
+      setTimeout(removerNaoClicavel, 1000);
+      setTimeout(virarCartasDiferentes, 1000);
     }
-    console.log(v);
+    contador = 0;
   }
-  
 }
 
 function virarCartasDiferentes(){
-  const viraVerso = document.querySelectorAll(".carta .verso-carta");
-  const viraFrente = document.querySelectorAll(".carta .frente-carta");
-  for(let i = 0 ; i < quantidadeCartas; i++){
-    viraFrente[i].classList.remove("ativar-frente");
-    viraVerso[i].classList.remove("ativar-verso");
-    console.log(i);
+  compararCarta1.classList.remove("ativar-frente");
+  compararCarta2.classList.remove("ativar-frente");
+  compararCarta1Verso.classList.remove("ativar-verso");
+  compararCarta2Verso.classList.remove("ativar-verso");
+
+  let primeiraCarta = document.querySelectorAll(".carta");
+  for(let i = 0; i < quantidadeCartas; i++){
+    primeiraCarta[i].classList.remove("nao-clicavel-primeira-carta");
+  }
+}
+
+function adicionarNaoClicavel(){
+  for(let i = 0; i < quantidadeCartas; i++){
+    let carta = document.querySelectorAll(".carta");
+    carta[i].classList.add("nao-clicavel");
+  }
+}
+
+function removerNaoClicavel(){
+  for(let i = 0; i < quantidadeCartas; i++){
+    let carta = document.querySelectorAll(".carta");
+    carta[i].classList.remove("nao-clicavel");
   }
 }
 
@@ -85,12 +106,12 @@ function adicionarCartas(){
   for(let i = 0; i < quantidadeCartas; i++){
   cartas.innerHTML += `
   <div class="carta" onclick="VirarCarta(this)" data-identifier="card">
-    <div class="verso-carta" data-identifier="front-face">
+    <div class="verso-carta" data-identifier="back-face">
       <img src="./imgs/front.png" alt="carta">
     </div>
 
-    <div class="frente-carta" data-identifier="back-face">
-      <img class="${copiaImagens[i]}" src="./imgs/${copiaImagens[i]}.gif" alt="carta">
+    <div class="frente-carta" data-identifier="front-face">
+      <img src="./imgs/${copiaImagens[i]}.gif" alt="carta">
     </div>
   </div> 
   `;
@@ -98,6 +119,11 @@ function adicionarCartas(){
 
 }
 
+function ganharPartida(){
+  if(contarPares === quantidadeCartas){
+    alert("Você ganhou em " + contadorJogadas + " jogadas!");
+  }
+}
 
 qualQuantidadeCartas();
 adicionarCartas();
